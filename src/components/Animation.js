00feed0e73d430336d33d16f042accbc72aafba4
John Fredriksson <93582861+johnfredriksson@users.noreply.github.com>
2023-08-08 19:01:15 +0200

    fixing fog

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -7,7 +7,9 @@ export default function Animation() {
         <a href="./">
             <img className="absolute top-5 left-5 w-[100px] transition-all hover:-rotate-45" src={logo} alt="Bella Leonette Logo" />
         </a>
-        <Spline scene="https://prod.spline.design/aQJ6IRxc1zjpFLZK/scene.splinecode" />
+        {/* <Spline scene="https://prod.spline.design/EPvntYYBRGvAPQuA/scene.splinecode" /> */}
+        <Spline scene="https://prod.spline.design/EChf4WPGk7XC6WWN/scene.splinecode" />
+        {/* <Spline scene="https://prod.spline.design/aQJ6IRxc1zjpFLZK/scene.splinecode" /> */}
     </section>
   )
 }
