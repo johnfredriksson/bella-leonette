b7213dd38df36d9ff63b125beea5e8e663536d9f
John Fredriksson <93582861+johnfredriksson@users.noreply.github.com>
2023-08-09 09:36:08 +0200

    updated layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,24 @@
 
 export default function Footer() {
   return (
-    <section className="min-h-[10vh] flex flex-col justify-center bg-zinc-400 tracking-widest">
-        <a className="text-white" href="https://open.spotify.com/artist/0Qzh6QfLR7zubyDl581ZOt?si=4mQQSqjBTpC3tOuODfBV8A">spotify</a>
-        <a className="text-white" href="https://www.instagram.com/bellaleonette/">instagram</a>
-        <p className="text-white">© en bella leonette produktion, 2023</p>
+    <section className="min-h-[10vh] bg-white">
+        <div className="h-4 border-b-[1px] border-zinc-100"></div>
+        <div className="h-4 border-b-[2px] border-zinc-100"></div>
+        <div className="h-4 border-b-[3px] border-zinc-100"></div>
+        <div className="h-4 border-b-[4px] border-zinc-100"></div>
+        <div className="h-4 border-b-[5px] border-zinc-200"></div>
+        <div className="h-4 border-b-[6px] border-zinc-200"></div>
+        <div className="h-4 border-b-[7px] border-zinc-200"></div>
+        <div className="h-4 border-b-[8px] border-zinc-200"></div>
+        <div className="h-4 border-b-[9px] border-zinc-200"></div>
+        <div className="h-4 border-b-[10px] border-zinc-300"></div>
+        <div className="h-4 border-b-[11px] border-zinc-300"></div>
+        <div className="h-4 border-b-[12px] mb-[3px] border-zinc-300"></div>
+        <div className="h-40 bg-zinc-300 flex flex-col justify-center tracking-widest">
+            <a className="text-white" href="https://open.spotify.com/artist/0Qzh6QfLR7zubyDl581ZOt?si=4mQQSqjBTpC3tOuODfBV8A">spotify</a>
+            <a className="text-white" href="https://www.instagram.com/bellaleonette/">instagram</a>
+            <p className="text-white">© en bella leonette produktion, 2023</p>
+        </div>
     </section>
   )
 }
